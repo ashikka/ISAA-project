bc66e0f53c84b7b47136ad864fda2fafd5de9c22
ashikka <ashikagupta28@gmail.com>
2021-08-20 21:49:34 +0530

    feat: Simple REST API

---
+++ src/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"ISAA-project/src/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	
+	r.GET("/ping", controllers.PingGet())
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
