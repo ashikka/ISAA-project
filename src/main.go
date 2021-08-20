31cd40a7aebbf34a5b121b94bfa1b93d3f2740e4
ashikka <ashikagupta28@gmail.com>
2021-08-20 22:28:09 +0530

    feat: GET route

--- src/main.go
+++ src/main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
-	"ISAA-project/src/controllers"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
+type blogPost struct {
+	ID      string `json:"id"`
+	Title   string `json:"title"`
+	Author  string `json:"author"`
+	Content string `json:"content"`
+}
+
+var blogPosts = []blogPost{
+	{ID: "1", Title: "Blue Train", Author: "John Coltrane", Content: "Something"},
+	{ID: "2", Title: "Jeru", Author: "Gerry Mulligan", Content: "Something"},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Author: "Sarah Vaughan", Content: "Something"},
+}
+
+func goDotEnvVariable(key string) string {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
 func main() {
-	r := gin.Default()
-	
-	r.GET("/ping", controllers.PingGet())
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	router := gin.Default()
+	router.GET("/blogs", getBlogs)
+
+	router.Run(goDotEnvVariable("BACKEND_URL"))
+}
+
+func getBlogs(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, blogPosts)
 }
