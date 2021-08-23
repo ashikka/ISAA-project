6d4566f5876b143994974291103b5e36e1bd6df3
ashikka <ashikagupta28@gmail.com>
2021-08-23 13:58:50 +0530

    feat: Used db functions

--- src/controllers/controllers.go
+++ src/controllers/controllers.go
@@ -1,32 +1,26 @@
 package controllers
 
 import (
+	"ISAA-project/src/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
-type blogPost struct {
-	ID      string `json:"id"`
-	Title   string `json:"title"`
-	Author  string `json:"author"`
-	Content string `json:"content"`
-}
-
-var blogPosts = []blogPost{
-	{ID: "1", Title: "Blue Train", Author: "John Coltrane", Content: "Something"},
-	{ID: "2", Title: "Jeru", Author: "Gerry Mulligan", Content: "Something"},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Author: "Sarah Vaughan", Content: "Something"},
-}
+var db *gorm.DB = models.ConnectDB("")
 
 func GetBlogs() gin.HandlerFunc {
+	var blogPosts []models.BlogPost
+
+	db.Find(&blogPosts)
 	return func(c *gin.Context) {
 		c.IndentedJSON(http.StatusOK, blogPosts)
 	}
 }
 
 func PostBlogs() gin.HandlerFunc {
-	var newBlog blogPost
+	var newBlog models.BlogPost
 
 	return func(c *gin.Context) {
 
@@ -34,22 +28,34 @@ func PostBlogs() gin.HandlerFunc {
 			return
 		}
 
-		blogPosts = append(blogPosts, newBlog)
+		db.Create(&newBlog)
 		c.IndentedJSON(http.StatusCreated, newBlog)
 	}
 }
 
 func GetBlogByID() gin.HandlerFunc {
+	var blogPosts []models.BlogPost
 
 	return func(c *gin.Context) {
 		id := c.Param("id")
 
-		for _, b := range blogPosts {
-			if b.ID == id {
-				c.IndentedJSON(http.StatusOK, b)
-				return
-			}
-		}
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "blog not found"})
+		db.First(&blogPosts, id)
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Blog not found"})
+	}
+}
+
+func DeleteBlogById() gin.HandlerFunc {
+	var blogPost models.BlogPost
+
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		db.First(&blogPost, id)
+		db.Delete(&blogPost)
+
+		var blogPosts []models.BlogPost
+		db.Find(&blogPosts)
+
+		c.IndentedJSON(http.StatusNotFound, blogPosts)
+
 	}
 }
