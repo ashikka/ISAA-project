4ba27ef47ea5227361889916e593aaa13bf0fab7
ashikka <ashikagupta28@gmail.com>
2021-08-23 13:59:01 +0530

    feat: Added new route

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	router.GET("/blogs", controllers.GetBlogs())
 	router.GET("/blogs/:id", controllers.GetBlogByID())
 	router.POST("/blogs", controllers.PostBlogs())
+	router.DELETE("/blogs/:id", controllers.DeleteBlogById())
 
 	router.Run(utils.GoDotEnvVariable("BACKEND_URL"))
 }
