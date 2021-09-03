c81fafb41a91788686fdbb7132cefd0ad7d40f2e
ashikka <ashikagupta28@gmail.com>
2021-09-03 20:17:53 +0530

    feat: Configured db connection

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"ISAA-project/src/controllers"
+	"ISAA-project/src/models"
 	"ISAA-project/src/utils"
+
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	models.ConnectDB()
 	router := gin.Default()
 	router.GET("/blogs", controllers.GetBlogs())
 	router.GET("/blogs/:id", controllers.GetBlogByID())
